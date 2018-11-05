aeb2df2c7b42b24ef7b3b483a686ff05e4c8dd52
Sebastien Cadorette <cado.94@me.com>
2018-11-05 13:48:08 -0500

    Ajustement du test d02. Accès aux attributs privés.

--- code/src/main/java/TP4/testTp4.java
+++ code/src/main/java/TP4/testTp4.java
@@ -2,6 +2,7 @@ package TP4;
 
 import edu.princeton.cs.algs4.Queue;
 import org.junit.Test;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 
 import java.util.NoSuchElementException;
@@ -17,21 +18,30 @@ public class testTp4 {
 
     @BeforeEach
     void setUp() throws Exception {
-
     }
+
+    @AfterEach
+    public void tearDown() throws Exception { }
 
     /**
      * d02 = < { Queue() -> size() } , { first == null, last == null, n == 0 } >
      */
     @Test
     public void d02() throws NoSuchFieldException, IllegalAccessException {
+        // Arrange
         queue = new Queue<Integer>();
         Field last = queue.getClass().getDeclaredField("last");
-        
         last.setAccessible(true);
-        assertNull(last.get(queue), "L'attribut last devrait etre null.");
-        assertEquals(queue.size(), 0, "La taille de la Queue devrait être de 0.");
-        assertTrue(queue.isEmpty(), "L'attribut first est null.");
+        Field first = queue.getClass().getDeclaredField("first");
+        first.setAccessible(true);
+
+        // Act
+        int resultat = queue.size();
+
+        // Assert
+        assertEquals(resultat, 0, "L'attribut n devrait être égal à 0.");
+        assertEquals(last.get(queue), null, "L'attribut last devrait être null.");
+        assertEquals(first.get(queue), null, "L'aatribut first devrait être null.");
     }
 
     /**
