c543d5dc10009589a00fd03f85402ab8c2ebdfbb
Sebastien Cadorette <cado.94@me.com>
2018-11-05 13:19:06 -0500

    Test d02 fait.

--- code/src/main/java/TP4/testTp4.java
+++ code/src/main/java/TP4/testTp4.java
@@ -4,7 +4,13 @@ import edu.princeton.cs.algs4.Queue;
 import org.junit.Test;
 import org.junit.jupiter.api.BeforeEach;
 
-import static junit.framework.TestCase.assertEquals;
+import java.util.NoSuchElementException;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 
 public class testTp4 {
     Queue queue;
@@ -14,10 +20,26 @@ public class testTp4 {
 
     }
 
+    /**
+     * d02 = < { Queue() -> size() } , { first == null, last == null, n == 0 } >
+     */
     @Test
-    public void premierTest() {
-        assertEquals( 0, 0);
+    public void d02() throws NoSuchFieldException, IllegalAccessException {
+        queue = new Queue<Integer>();
+        Field last = queue.getClass().getDeclaredField("last");
+        
+        last.setAccessible(true);
+        assertNull(last.get(queue), "L'attribut last devrait etre null.");
+        assertEquals(queue.size(), 0, "La taille de la Queue devrait être de 0.");
+        assertTrue(queue.isEmpty(), "L'attribut first est null.");
     }
 
+    /**
+     * d04 = < { Queue() -> peek() } , { first == null, last == null, n == 0 } >
+     */
+    @Test
+    public void d04() {
+        assertEquals(0,0);
+    }
 
 }
