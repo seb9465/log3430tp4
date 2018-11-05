e5237f6a528f107145b6339548d412c2518ae89e
J-F <jffontaine_@hotmail.com>
2018-11-05 11:47:55 -0500

    premier test

---
+++ code/src/tp4/testTp4.java
@@ -0,0 +1,21 @@
+import edu.princeton.cs.algs4.Queue;
+import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
+
+import static junit.framework.TestCase.assertEquals;
+
+public class testTp4 {
+    Queue queue;
+
+    @BeforeEach
+    void setUp() throws Exception {
+
+    }
+
+    @Test
+    public void premierTest() {
+        assertEquals( 0, 0);
+    }
+
+
+}
